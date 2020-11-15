727a37e9ec5ab907e0af38cb93e24a6df93900ad
18610629040 <651688452@qq.com>
2020-11-15 16:33:07 +0800

    日日日

--- src/main/java/com/dity/controller/AuthController.java
+++ src/main/java/com/dity/controller/AuthController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -35,6 +36,16 @@ public class AuthController {
 	
 	@Autowired
 	TokenService tokenService;
+	
+	@RequestMapping(value = "/gotoLogin", method = { RequestMethod.POST, RequestMethod.GET })
+    public String gotoLogin(){
+        return "/login/login";
+    }
+	
+	@RequestMapping(value = "/gotoRegister", method = { RequestMethod.POST, RequestMethod.GET })
+    public String gotoRegister(){
+        return "/login/register";
+    }
 	
     @RequestMapping("/notLogin")
     @ResponseBody
