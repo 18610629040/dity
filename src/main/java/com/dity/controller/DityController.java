0952c2d61dce5c6a1619a3df75cb0047da8bb341
18610629040 <651688452@qq.com>
2020-11-15 17:57:26 +0800

    q'we'e

--- src/main/java/com/dity/controller/DityController.java
+++ src/main/java/com/dity/controller/DityController.java
@@ -1,13 +1,16 @@
 package com.dity.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@CrossOrigin
 @Controller
+@RequestMapping("/dity")
 public class DityController {
     @RequestMapping(value = "/index", method = { RequestMethod.POST, RequestMethod.GET })
     public String holle(){
-        return "/index";
+        return "/index/index";
     }
 }
