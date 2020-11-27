2be87b1fc26a8e3bec3d8de20aef3b76b63f6e43
18610629040 <651688452@qq.com>
2020-11-27 21:06:12 +0800

    weqw

--- src/main/java/com/dity/DityApplication.java
+++ src/main/java/com/dity/DityApplication.java
@@ -2,8 +2,12 @@ package com.dity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@ServletComponentScan
+@EnableAsync
 public class DityApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DityApplication.class, args);
